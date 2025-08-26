0eed5770410c56957e541e725add1b75e80dbeba
lzj <lzj15146109392@163.com>
2025-08-26 10:58:59 +0800

    8 26

--- gulimall-product/src/main/java/com/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/gulimall/product/GulimallProductApplication.java
@@ -2,8 +2,13 @@ package com.gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.gulimall.product",  // 业务模块自身的包
+        "com.gulimall.common"    // 公共模块的包（包含 RedisConfig）
+})
 public class GulimallProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallProductApplication.class, args);
